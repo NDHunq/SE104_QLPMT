0d038190100b03f82d6c02aa90fa23db946691af
hdpiscoding <22520339@gm.uit.edu.vn>
2024-04-28 23:14:48 +0700

    hahaha

--- QLPMT/src/main/java/com/example/qlpmt/benh_nhanController.java
+++ QLPMT/src/main/java/com/example/qlpmt/benh_nhanController.java
@@ -27,16 +27,26 @@ public class benh_nhanController implements Initializable{
 
     private ObservableList<PhieuKhamBenh> pkb_list;
 
+    //Ham khoi tao khi khoi dong
     public void initialize(URL location, ResourceBundle resources){
         setupPaginated();
 
-        pkb.autosizeColumnsOnInitialization();
+        //chia deu kich thuoc cac cot de vua voi chieu rong cua tableview
+        double tableViewWidth = pkb.getPrefWidth();
+        int numberOfColumns = pkb.getTableColumns().size();
+        for (MFXTableColumn column : pkb.getTableColumns()) {
+            column.setPrefWidth(tableViewWidth / numberOfColumns);
+        }
 
+        //pkb.autosizeColumnsOnInitialization();
+
+        //Tu dong dieu chinh kich thuoc cac cot de phu hop voi noi dung
         When.onChanged(pkb.currentPageProperty())
                 .then((oldValue, newValue) -> pkb.autosizeColumns())
                 .listen();
     }
 
+    //Ham khoi tao du lieu mau
     public void setData(){
         pkb_list = FXCollections.observableArrayList(
                 new PhieuKhamBenh("09010235718", "Nguyen Van A", "Ung thu co tu cung", "Dai ra mau", LocalDate.parse("12/07/2023", formatter)),
@@ -78,21 +88,27 @@ public class benh_nhanController implements Initializable{
         );
     }
 
-
+    //Ham khoi tao paginated tableview
     private void setupPaginated() {
+
+        //Tao cac cot cua tableview
         MFXTableColumn<PhieuKhamBenh> cccd = new MFXTableColumn<>("CCCD", false, Comparator.comparing(PhieuKhamBenh::getCccd));
         MFXTableColumn<PhieuKhamBenh> hoten = new MFXTableColumn<>("Ho Ten", false, Comparator.comparing(PhieuKhamBenh::getHoTen));
         MFXTableColumn<PhieuKhamBenh> loaibenh = new MFXTableColumn<>("Loai benh", false, Comparator.comparing(PhieuKhamBenh::getLoaiBenh));
         MFXTableColumn<PhieuKhamBenh> trieuchung = new MFXTableColumn<>("Trieu chung", false, Comparator.comparing(PhieuKhamBenh::getTrieuChung));
         MFXTableColumn<PhieuKhamBenh> ngaykham = new MFXTableColumn<>("Ngay kham", false, Comparator.comparing(PhieuKhamBenh::getNgayKham_string));
 
-        cccd.setRowCellFactory(device -> new MFXTableRowCell<>(PhieuKhamBenh::getCccd));
-        hoten.setRowCellFactory(device -> new MFXTableRowCell<>(PhieuKhamBenh::getHoTen));
-        loaibenh.setRowCellFactory(device -> new MFXTableRowCell<>(PhieuKhamBenh::getLoaiBenh));
-        trieuchung.setRowCellFactory(device -> new MFXTableRowCell<>(PhieuKhamBenh::getTrieuChung));
-        ngaykham.setRowCellFactory(device -> new MFXTableRowCell<>(PhieuKhamBenh::getNgayKham_string));
+        //Tao cac dong cho cot cua tableview
+        cccd.setRowCellFactory(phieukhambenh -> new MFXTableRowCell<>(PhieuKhamBenh::getCccd));
+        hoten.setRowCellFactory(phieukhambenh -> new MFXTableRowCell<>(PhieuKhamBenh::getHoTen));
+        loaibenh.setRowCellFactory(phieukhambenh -> new MFXTableRowCell<>(PhieuKhamBenh::getLoaiBenh));
+        trieuchung.setRowCellFactory(phieukhambenh -> new MFXTableRowCell<>(PhieuKhamBenh::getTrieuChung));
+        ngaykham.setRowCellFactory(phieukhambenh -> new MFXTableRowCell<>(PhieuKhamBenh::getNgayKham_string));
 
+        //Them cac cot vao tableview
         pkb.getTableColumns().addAll(cccd, hoten, loaibenh, trieuchung, ngaykham);
+
+        //Them cac filter cho tableview
         pkb.getFilters().addAll(
                 new StringFilter<>("CCCD", PhieuKhamBenh::getCccd),
                 new StringFilter<>("Ho ten", PhieuKhamBenh::getHoTen),
@@ -100,6 +116,8 @@ public class benh_nhanController implements Initializable{
                 new StringFilter<>("Trieu chung", PhieuKhamBenh::getTrieuChung),
                 new StringFilter<>("Ngay kham", PhieuKhamBenh::getNgayKham_string)
         );
+
+        //Them du lieu vao tableview
         setData();
         pkb.setItems(pkb_list);
     }
