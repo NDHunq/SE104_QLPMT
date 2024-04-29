5d2cc67e06967c0cb62611bff2d1f5b442d9a4ee
hdpiscoding <22520339@gm.uit.edu.vn>
2024-04-29 17:46:27 +0700

    hihihi

--- QLPMT/src/main/java/com/example/qlpmt/benh_nhanController.java
+++ QLPMT/src/main/java/com/example/qlpmt/benh_nhanController.java
@@ -3,14 +3,25 @@ package com.example.qlpmt;
 import Model.PhieuKhamBenh;
 import io.github.palexdev.materialfx.controls.MFXPaginatedTableView;
 import io.github.palexdev.materialfx.controls.MFXTableColumn;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
 import io.github.palexdev.materialfx.filter.StringFilter;
 import io.github.palexdev.materialfx.utils.others.observables.When;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.text.Text;
 
+import java.awt.*;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -20,8 +31,12 @@ import java.util.ResourceBundle;
 
 public class benh_nhanController implements Initializable{
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.ENGLISH);
-    DateTimeFormatter string_formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
+    @FXML
+    private Text soBenhNhan;
+    @FXML
+    private AnchorPane benh_nhan_root_node;
+    @FXML
+    private MFXTextField search_txtbox;
     @FXML
     private MFXPaginatedTableView<PhieuKhamBenh> pkb;
 
@@ -29,6 +44,7 @@ public class benh_nhanController implements Initializable{
 
     //Ham khoi tao khi khoi dong
     public void initialize(URL location, ResourceBundle resources){
+        //Khoi tao paginated tableview
         setupPaginated();
 
         //chia deu kich thuoc cac cot de vua voi chieu rong cua tableview
@@ -38,12 +54,28 @@ public class benh_nhanController implements Initializable{
             column.setPrefWidth(tableViewWidth / numberOfColumns);
         }
 
+        ////Tu dong dieu chinh kich thuoc cac cot de phu hop voi noi dung khi vua khoi tao
         //pkb.autosizeColumnsOnInitialization();
 
-        //Tu dong dieu chinh kich thuoc cac cot de phu hop voi noi dung
+        //Tu dong dieu chinh kich thuoc cac cot de phu hop voi noi dung khi co thay doi
         When.onChanged(pkb.currentPageProperty())
                 .then((oldValue, newValue) -> pkb.autosizeColumns())
                 .listen();
+
+        // Them su kien unfocus cho search_txtbox khi click ra ngoai bang cach requestFocus den node khac
+        benh_nhan_root_node.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                // Check if the clicked target is not the MFXTextField
+                if (mouseEvent.getTarget() != search_txtbox) {
+                    // If it's not, request focus on the root node
+                    benh_nhan_root_node.requestFocus();
+                }
+            }
+        });
+
+        // Cap nhat so luong benh nhan luc khoi tao
+        soBenhNhan.setText(String.valueOf(pkb_list.size()));
     }
 
     //Ham khoi tao du lieu mau
