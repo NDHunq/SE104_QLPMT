9d1c851dde31151fd13f2e96ba9edc6ae4239ae9
PhamKhaiHung <22520522@gm.uit.edu.vn>
2024-07-01 14:55:11 +0700

    sua rang buoc

--- QLPMT/src/main/java/com/example/qlpmt/DBConnection.java
+++ QLPMT/src/main/java/com/example/qlpmt/DBConnection.java
@@ -14,12 +14,12 @@ public class DBConnection {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             // ten database
 
-//            String url = "jdbc:sqlserver://LAPTOP-CL3NH660:1433;databaseName=QLPK";
-//            String user = "sa"; // replace with your username
-//            String password = "phamkhaihung123"; // replace with your password
-            String url = "jdbc:sqlserver://DESKTOP-MR3E6H4\\SQLEXPRESS:1433;databaseName=QLPK";
+            String url = "jdbc:sqlserver://LAPTOP-CL3NH660:1433;databaseName=QLPK";
             String user = "sa"; // replace with your username
-            String password = "hhhhhhhh444"; // replace with your password
+            String password = "phamkhaihung123"; // replace with your password
+//            String url = "jdbc:sqlserver://DESKTOP-MR3E6H4\\SQLEXPRESS:1433;databaseName=QLPK";
+//            String user = "sa"; // replace with your username
+//            String password = "hhhhhhhh444"; // replace with your password
             connection = (Connection) DriverManager.getConnection(url, user, password);
 
 
